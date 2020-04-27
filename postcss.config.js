a7667b3aefa3f745dddf2ffe30bb5301cec7983b
Kim Songhyun <yeonmu4121@gmail.com>
2020-04-27 20:26:00 +0900

    Use PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = ({ file, options, env }) => ({
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        purgecss({
+            content: [
+                './layouts/**/*.pug',
+                './src/**/*.vue',
+            ],
+        }),
     ],
 });
