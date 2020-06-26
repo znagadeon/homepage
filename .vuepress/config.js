bf1f042afef67ae2e444c6c4d126c606be5e8881
Kim Songhyun <yeonmu4121@gmail.com>
2020-06-26 23:14:46 +0900

    Add blog and wiki sections

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: '지나가던 개발자',
+
+    port: 1337,
+
+    themeConfig: {
+        nav: [
+            { text: 'Blog', link: '/blog/' },
+            { text: 'Wiki', link: '/wiki/' },
+        ],
+        sidebar: {
+            '/': 'auto',
+        },
+    },
+};
