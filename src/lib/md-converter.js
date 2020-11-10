74844c85117d5f838b7aeec5765edb4a3aa0314c
Kim Songhyun <yeonmu4121@gmail.com>
2020-11-10 19:33:47 +0900

    Add katex

--- src/lib/md-converter.js
+++ src/lib/md-converter.js
@@ -1,4 +1,5 @@
 const marked = require('marked');
+const katex = require('katex');
 
 const { link, image, code, codespan } = require('./convert-rules');
 
@@ -11,5 +12,10 @@ renderer.codespan = codespan;
 marked.setOptions({ renderer });
 
 module.exports = {
-    md2html: markdown => marked(markdown),
+    md2html: markdown => {
+        const katexParsed = markdown.replace(/\\\(([^\$\n]+?)\\\)/g, (match, capture) => {
+            return katex.renderToString(capture);
+        });
+        return marked(katexParsed);
+    },
 };
