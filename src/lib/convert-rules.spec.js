f2e8a08babf8146c48b522f4abe79e25b7903d9f
Kim Songhyun <yeonmu4121@gmail.com>
2021-04-06 18:30:09 +0900

    Remove convert-rules.spec.js

--- src/lib/convert-rules.spec.js
+++
@@ -1,3 +0,0 @@
-describe('stub', () => {
-	it('stub', () => {});
-});
--- src/routers/post.js
+++ src/routers/post.js
@@ -16,25 +16,15 @@ const renderer = createBundleRenderer(bundle, {
 post.get('/', (req, res) => {
 	res.redirect(301, '/index.html');
 });
-
-post.get('/post/:title', (req, res) => {
-	res.redirect(301, `/post/${req.params.title}/index.html`);
-});
-
-post.get('/archive', (req, res) => {
-	res.redirect(301, '/archive/index.html');
-});
-
-post.get('/tag/:tag', (req, res) => {
-	res.redirect(301, `/tag/${req.params.tag}/index.html`);
-});
-
 post.get('/index.html', async (req, res) => {
 	res.send(await renderer.renderToString({
 		type: 'Home',
 	}));
 });
 
+post.get('/post/:title', (req, res) => {
+	res.redirect(301, `/post/${req.params.title}/index.html`);
+});
 post.get('/post/:title/index.html', async (req, res) => {
 	res.send(await renderer.renderToString({
 		type: 'Post',
@@ -42,12 +32,18 @@ post.get('/post/:title/index.html', async (req, res) => {
 	}));
 });
 
+post.get('/archive', (req, res) => {
+	res.redirect(301, '/archive/index.html');
+});
 post.get('/archive/index.html', async (req, res) => {
 	res.send(await renderer.renderToString({
 		type: 'Archive',
 	}));
 });
 
+post.get('/tag/:tag', (req, res) => {
+	res.redirect(301, `/tag/${req.params.tag}/index.html`);
+});
 post.get('/tag/:tag/index.html', async (req, res) => {
 	res.send(await renderer.renderToString({
 		type: 'Tag',
