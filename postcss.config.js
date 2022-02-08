311bf5a013871da831e9078d9e9b3153c1799337
Kim Songhyun <yeonmu4121@gmail.com>
2022-02-08 17:23:14 +0900

    Remove purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-
 module.exports = ({ _file, _options, _env }) => ({
 	plugins: [
 		require('tailwindcss'),
 		require('autoprefixer'),
-		purgecss({
-			content: ['./layouts/**/*.pug', './src/**/*.vue'],
-			whitelistPatternsChildren: [/article/],
-		}),
 	],
 });
