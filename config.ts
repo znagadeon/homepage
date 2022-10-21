9c74806ce950369e80bf8f43ad284607843a2b12
Kim Songhyun <yeonmu4121@gmail.com>
2022-10-22 01:39:53 +0900

    Add type to config

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const blogName = '지나가던 개발자';
 export const name = 'Kim Songhyun';
 export const description = '카카오 프론트엔드 개발자 since 2018. 01.';
-export const host = 'https =//znagadeon.dev';
+export const host = 'https://znagadeon.dev';
 export const googleSearch = '981b407e9acf0f1f0';
 export const googleAnalytics = 'G-TP8Y4MDDCD';
 export const links = {
@@ -10,7 +10,7 @@ export const links = {
 	linkedin: 'kim-songhyun',
 	twitter: 'znagadeon',
 	gravatar: 'c1db11b57685a2fc71655a869cdac430',
-	donation: 'https =//toss.me/znagadeon'
+	donation: 'https://toss.me/znagadeon'
 };
 export const comment = {
 	repository: 'znagadeon/homepage',
