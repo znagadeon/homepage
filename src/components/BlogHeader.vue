ef1bd98667f4039bf24baafe830d4808798a5153
Kim Songhyun <yeonmu4121@gmail.com>
2023-02-01 21:50:41 +0900

    Migrate Search component

--- src/components/BlogHeader.vue
+++ src/components/BlogHeader.vue
@@ -4,53 +4,31 @@
 		<h1 class="title__blog-name">
 			<a href="/">{{ blogName }}</a>
 		</h1>
-		<div class="title__search search">
-			<label>
-				<span class="sr-only">Search</span>
-				<input type="text" class="search__input" v-model="query" @keyup.enter="search">
-			</label>
-			<button @click="search" aria-label="Search">
-				<icon name="search" :size="20"></icon>
-			</button>
-		</div>
+    <div ref="search"></div>
 	</div>
   <div ref="profile"></div>
 </header>
 </template>
 
 <script>
-import { ref } from 'vue';
 import { createRoot } from 'react-dom/client';
+import { Search } from './Search';
 import { Profile } from './Profile';
 import { blogName } from '@root/config';
 
-import Icon from './Icon.vue';
-
 export default {
-	components: {
-		Icon,
-	},
-
-	setup() {
-		const query = ref('');
-
-		const search = async () => {
-			if (!query.value) {
-				alert('검색어를 입력하세요');
-				return;
-			}
-
-			location.href = `/search/index.html?q=${query.value}`;
-		};
-
-		return { query, blogName, search };
-	},
-
-	created() {
-		this.query = this.$route.query.q || '';
-	},
+  data() {
+    return {
+      blogName,
+    }
+  },
 
   mounted() {
+    const searchRoot = createRoot(this.$refs.search);
+    searchRoot.render(Search({
+      className: 'title__search',
+    }));
+
     const profileRoot = createRoot(this.$refs.profile);
     profileRoot.render(Profile({
       className: 'header__profile',
@@ -89,39 +67,6 @@ export default {
   &__blog-name {
     @apply text-2xl;
     @apply font-bold;
-  }
-}
-
-.search {
-  @media (max-width: 600px) {
-    & {
-      @apply hidden;
-    }
-  }
-  @media (min-width: 600px) {
-    & {
-      @apply flex;
-      @apply flex-row;
-      @apply items-center;
-    }
-  }
-
-  &__input {
-    @apply border-0;
-    @apply border-b;
-    @apply border-gray-400;
-    @apply w-40;
-    @apply h-8;
-    @apply mr-3;
-
-    @apply bg-transparent;
-  }
-
-  &__submit {
-    @apply p-2;
-    @apply border-0;
-
-    @apply bg-transparent;
   }
 }
 </style>
---
+++ src/components/Search/index.tsx
@@ -0,0 +1,37 @@
+import { Icon } from '@src/components/Icon';
+import { HTMLAttributes, KeyboardEventHandler } from 'react';
+
+import styles from './style.module.scss';
+
+export const Search = ({ className }: HTMLAttributes<Record<string, never>>) => {
+  const search = () => {
+    const input = document.getElementById('searchInput') as HTMLInputElement;
+
+    if (!input.value) {
+      alert('검색어를 입력하세요');
+      return;
+    }
+
+    location.href = `/search/index.html?q=${input.value}`;
+  };
+
+  const searchUsingKeyboard: KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key !== 'Enter') return;
+    search();
+  };
+
+  // TODO: useEffect url query
+
+  return (
+    <div className={`${className} ${styles.search}`}>
+      <label>
+        <span className="sr-only">Search</span>
+        {/* FIXME: onChange */}
+        <input id="searchInput" type="text" className={styles.search__input} v-model="query" onKeyUp={searchUsingKeyboard} />
+      </label>
+      <button onClick={search} aria-label="Search">
+        <Icon name="search" size={20} />
+      </button>
+    </div>
+  );
+};
