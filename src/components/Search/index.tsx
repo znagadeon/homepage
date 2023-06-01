c2bab8f0f433084e6f9a5c27615aa7cf4eb774ee
Kim Songhyun <yeonmu4121@gmail.com>
2023-06-01 21:29:16 +0900

    Refactor Search component to use react hooks

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,19 +1,25 @@
 import { Icon } from '@src/components/Icon';
-import { HTMLAttributes, KeyboardEventHandler } from 'react';
+import { HTMLAttributes, KeyboardEventHandler, useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 import styles from './style.module.scss';
 import rootStyles from '@src/style.module.scss';
 
 export const Search = ({ className }: HTMLAttributes<Record<string, never>>) => {
-  const search = () => {
-    const input = document.getElementById('searchInput') as HTMLInputElement;
+  const [query, setQuery] = useState('');
+  const [searchParams] = useSearchParams();
 
-    if (!input.value) {
+  const onChange = (q: string) => {
+    setQuery(q);
+  };
+
+  const search = () => {
+    if (!query) {
       alert('검색어를 입력하세요');
       return;
     }
 
-    location.href = `/search/index.html?q=${input.value}`;
+    location.href = `/search/index.html?q=${query}`;
   };
 
   const searchUsingKeyboard: KeyboardEventHandler<HTMLInputElement> = (e) => {
@@ -21,14 +27,18 @@ export const Search = ({ className }: HTMLAttributes<Record<string, never>>) => 
     search();
   };
 
-  // TODO: useEffect url query
+  useEffect(() => {
+    const initialQuery = searchParams.get('q');
+    if (!initialQuery) return;
+
+    setQuery(initialQuery);
+  }, []);
 
   return (
     <div className={`${className} ${styles.search}`}>
       <label>
         <span className={rootStyles.srOnly}>Search</span>
-        {/* FIXME: onChange */}
-        <input id="searchInput" type="text" className={styles.search__input} v-model="query" onKeyUp={searchUsingKeyboard} />
+        <input type="text" className={styles.search__input} value={query} onChange={e => onChange(e.target.value)} onKeyUp={searchUsingKeyboard} />
       </label>
       <button onClick={search} aria-label="Search">
         <Icon name="search" size={20} />
