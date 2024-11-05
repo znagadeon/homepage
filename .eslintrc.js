cdbd19dab609fdf3cebdb12bbea9fee09be98ba5
Kim Songhyun <yeonmu4121@gmail.com>
2024-11-05 17:12:38 +0900

    Update indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-		'jest/globals': true,
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:vue/essential',
-	],
-	parserOptions: {
-		ecmaVersion: 12,
-		sourceType: 'module',
-	},
-	plugins: ['vue', 'jest'],
-	rules: {
-		'no-unused-vars': ['error', {'vars': 'all', 'args': 'after-used', 'argsIgnorePattern': '^_'}],
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-	},
-	globals: {
-		'IS_DEV': true,
-	},
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'jest'],
+  rules: {
+    'no-unused-vars': ['error', {'vars': 'all', 'args': 'after-used', 'argsIgnorePattern': '^_'}],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+  globals: {
+    'IS_DEV': true,
+  },
 };
