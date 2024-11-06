9a70000e3164f045a3c20aec481d27864e742d37
Kim Songhyun <yeonmu4121@gmail.com>
2024-11-06 15:34:47 +0900

    Replace jest into vitest

--- src/lib/get-posts.spec.js
+++ src/lib/get-posts.spec.js
@@ -1,42 +1,43 @@
-const mock = require('mock-fs');
+import {describe, it, expect, afterEach} from 'vitest';
+import mock from 'mock-fs';
 
 const getPosts = require('./get-posts');
 
 describe('getPosts', () => {
-	it('returns page info list and assets', () => {
-		mock({
-			contents: {
-				'simple-page.md': '',
-				'complex-page': {
-					assets: {
-						'asset-1.png': '',
-						'asset-2.png': '',
-					},
-					'index.md': '',
-				},
-			},
-		});
+  it('returns page info list and assets', () => {
+    mock({
+      contents: {
+        'simple-page.md': '',
+        'complex-page': {
+          assets: {
+            'asset-1.png': '',
+            'asset-2.png': '',
+          },
+          'index.md': '',
+        },
+      },
+    });
 
-		const expectedPages = [
-			'contents/simple-page.md',
-			'contents/complex-page/index.md',
-		];
-		const pages = getPosts('./contents');
+    const expectedPages = [
+      'contents/simple-page.md',
+      'contents/complex-page/index.md',
+    ];
+    const pages = getPosts('./contents');
 
-		expect(pages).toEqual(expect.arrayContaining(expectedPages));
-		expect(expectedPages).toEqual(expect.arrayContaining(pages));
-	});
+    expect(pages).toEqual(expect.arrayContaining(expectedPages));
+    expect(expectedPages).toEqual(expect.arrayContaining(pages));
+  });
 
-	it('ignores unnecessary files', () => {
-		mock({
-			'.DS_Store': '',
-		});
+  it('ignores unnecessary files', () => {
+    mock({
+      '.DS_Store': '',
+    });
 
-		const pages = getPosts('./');
-		expect(pages).toHaveLength(0);
-	});
+    const pages = getPosts('./');
+    expect(pages).toHaveLength(0);
+  });
 
-	afterEach(() => {
-		mock.restore();
-	});
+  afterEach(() => {
+    mock.restore();
+  });
 });
