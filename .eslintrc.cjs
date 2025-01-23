f78b0e8a6338df946b90554165eaa1df5df9a5a3
Kim Songhyun <yeonmu4121@gmail.com>
2025-01-23 20:01:44 +0900

    Set react lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,20 +7,26 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:vue/essential',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
+    ecmaFeatures: {
+      jsx: true,
+    },
     sourceType: 'module',
   },
-  plugins: ['vue'],
+  plugins: ['vue', 'react'],
   rules: {
     'no-unused-vars': ['error', {'vars': 'all', 'args': 'after-used', 'argsIgnorePattern': '^_'}],
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 'off',
+
   },
   overrides: [
     {
