848ee0e7d538890d8534fc51b6c59b78921e3d66
Kim Songhyun <yeonmu4121@gmail.com>
2025-02-09 16:15:15 +0900

    Remove duplicate flag

--- src/router.js
+++ src/router.js
@@ -1,29 +1,36 @@
-import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router';
+import {
+  createMemoryHistory,
+  createRouter,
+  createWebHistory,
+} from 'vue-router';
 
 import Home from '@src/pages/Home.vue';
 
-import Post from '@src/pages/Post.vue';
 import Archive from '@src/pages/Archive.vue';
+import Post from '@src/pages/Post.vue';
 import Tag from '@src/pages/Tag.vue';
 
 import Search from '@src/pages/Search.vue';
 
 export default () => {
-	const isServer = typeof window === 'undefined';
-	return createRouter({
-		history: isServer ? createMemoryHistory() : createWebHistory(),
-		routes: [
-			{ path: '/index.html', component: Home, alias: '/' },
+  return createRouter({
+    history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
+    routes: [
+      { path: '/index.html', component: Home, alias: '/' },
 
-			{ path: '/post/:title/index.html', component: Post, alais: '/post/:title' },
-			{ path: '/archive/index.html', component: Archive, alias: '/archive' },
-			{ path: '/tag/:tag/index.html', component: Tag, alias: '/tag/:tag' },
+      {
+        path: '/post/:title/index.html',
+        component: Post,
+        alais: '/post/:title',
+      },
+      { path: '/archive/index.html', component: Archive, alias: '/archive' },
+      { path: '/tag/:tag/index.html', component: Tag, alias: '/tag/:tag' },
 
-			{
-				path: '/search/index.html',
-				component: Search,
-				props: (route) => ({ query: route.query.q }),
-			},
-		],
-	});
+      {
+        path: '/search/index.html',
+        component: Search,
+        props: (route) => ({ query: route.query.q }),
+      },
+    ],
+  });
 };
