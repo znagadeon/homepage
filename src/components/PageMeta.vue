2aa813c6c20b0b5b507ba34353db7e66b3380963
Kim Songhyun <yeonmu4121@gmail.com>
2025-02-19 16:55:54 +0900

    Add v-if

--- src/components/PageMeta.vue
+++ src/components/PageMeta.vue
@@ -1,15 +1,15 @@
 <template>
-	<title>{{ meta.title }}</title>
+  <title v-if="meta.title">{{ meta.title }}</title>
 
-	<meta name="author" :content="meta.author">
-	<meta name="description" :content="meta.description">
+  <meta v-if="meta.author" name="author" :content="meta.author">
+  <meta v-if="meta.description" name="description" :content="meta.description">
 
-	<meta property="og:site_name" :content="meta.title">
-	<meta property="og:type" :content="meta.opengraph.type">
-	<meta property="og:url" :content="meta.opengraph.url">
-	<meta property="og:title" :content="meta.opengraph.title">
-	<meta property="og:description" :content="meta.opengraph.description">
-	<meta property="og:image" :content="meta.opengraph.image">
+  <meta v-if="meta.title" property="og:site_name" :content="meta.title">
+  <meta v-if="meta.opengraph.type" property="og:type" :content="meta.opengraph.type">
+  <meta v-if="meta.opengraph.url" property="og:url" :content="meta.opengraph.url">
+  <meta v-if="meta.opengraph.title" property="og:title" :content="meta.opengraph.title">
+  <meta v-if="meta.opengraph.description" property="og:description" :content="meta.opengraph.description">
+  <meta v-if="meta.opengraph.image" property="og:image" :content="meta.opengraph.image">
 </template>
 
 <script>
