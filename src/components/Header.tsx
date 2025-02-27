752cd7f8bc379d20c6f71561347bcf799fb56b41
Kim Songhyun <yeonmu4121@gmail.com>
2025-02-27 16:15:33 +0900

    Update header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,9 @@ import style from './Header.module.scss';
 export const Header = () => {
   return (
     <header className={style.header}>
-      <div className={style.title}>
-        <h1 className={style['title__blog-name']}>
-          <a href="/">{config.blogName}</a>
-        </h1>
-      </div>
+      <h1 className={style['header__blog-name']}>
+        <a href="/">{config.blogName}</a>
+      </h1>
     </header>
   );
 };
